8b3652af616144204dcd884e149b3ffd264ea768
Zilu <zw427@cornell.edu>
2024-10-31 10:05:37 -0400

    change nproc-per-node from 3 to 4

--- cnn/cnn_part5_run_distributed.sh
+++ cnn/cnn_part5_run_distributed.sh
@@ -14,7 +14,7 @@ fi
 
 
 PRELOAD="/opt/apps/tacc-apptainer/1.1.8/bin/apptainer exec --nv --bind /run/user:/run/user /work2/10000/zw427/cnn_course_latest.sif "
-CMD="python3 -m torch.distributed.run --nproc_per_node 3 --nnodes $NNODES --node_rank=$LOCAL_RANK --master_addr=$MAIN_RANK --master_port=1234 cnn_part5_torch_train_distributed.py $@"
+CMD="python3 -m torch.distributed.run --nproc_per_node 4 --nnodes $NNODES --node_rank=$LOCAL_RANK --master_addr=$MAIN_RANK --master_port=1234 cnn_part5_torch_train_distributed.py $@"
 
 FULL_CMD="$PRELOAD $CMD"
 echo "Training command: $FULL_CMD"
